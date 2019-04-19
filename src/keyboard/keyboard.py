8c26ac10a7e27810a2bdcba1f3fdfc11dc0e0b4c
IvanYankovskiy <igyankovskiy@gmail.com>
2019-04-20 00:49:58 +0700

    Two-lines special keyboard. Implemented adjusting over ratio. Need to create navigation and edit buttons.

---
+++ src/keyboard/keyboard.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+
+class KeyboardBasis(object):
+    def __init__(self, parameters):
+        self.width = parameters['width']
+        self.height = parameters['height']
+        self.un = parameters['un']
+        self.keyboard_background = np.zeros((self.height, self.width, self.un), np.uint8)
+
+        self.letter_rectangle_settings = parameters.get('reactangle')
+        self.letter_settings = parameters.get('letter')
+
+    def get_background(self):
+        return self.keyboard_background
+
+    def show_keyboard(self):
+        self.farme = cv2.imshow("keyboard", self.keyboard_background)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def set_letter_placeholder(self, parameters):
+        for order in range(7):
+            rectangle = TrueRectangle(parameters)
+            rel_y = int(self.height / 2) - rectangle.height
+            rel_x = order * rectangle.width
+            rectangle.show_rectangle(self.get_background(), rel_x, rel_y)
+
+    def set_letters(self, ph_parameters, letter_parameters, letters):
+        max_inline_rectangle_num = self.width // ph_parameters.get('width')
+        max_lines_num = 2
+        letter_counter = 0
+        for line_order in range(max_lines_num):
+            for order in range(max_inline_rectangle_num):
+                rectangle = TrueRectangle(ph_parameters)
+
+                rel_y = int(self.height / 2) - rectangle.height + rectangle.height * line_order
+                rel_x = order * rectangle.width
+                rectangle.show_rectangle(self.get_background(), rel_x, rel_y)
+
+                letter = Letter(letters[letter_counter], letter_parameters)
+                letter.show_letter(self.get_background(), rectangle, rel_x, rel_y)
+                letter_counter += 1
+
+#we are only interested in related coordinates after drawing
+class Rectangle(object):
+    def __init__(self, top_left_x, top_left_y, bottom_right_x, bottom_right_y, letter):
+        self.top_left_x = top_left_x
+        self.top_left_y = top_left_y
+        self.bottom_right_x = bottom_right_x
+        self.bottom_right_y = bottom_right_y
+        self.text = letter
+
+
+class TrueRectangle(object):
+    def __init__(self, parameters):
+        self.width = parameters.get('width')
+        self.height = parameters.get('height')
+        self.th = parameters.get('thickness')
+        self.color = parameters.get('color')
+
+    def show_rectangle(self, source, rel_x0, rel_y0):
+        cv2.rectangle(source,
+                      (rel_x0 + self.th, rel_y0 + self.th),
+                      (rel_x0 + self.width - self.th, rel_y0 + self.height - self.th),
+                      (255, 0, 0),
+                      self.th)
+
+
+class Letter(object):
+    def __init__(self, text, parameters):
+        self.text = text
+        self.font_letter = cv2.FONT_HERSHEY_PLAIN
+        self.font_scale = parameters.get('font_scale')
+        self.font_thickness = parameters.get('font_thickness')
+        self.text_size = cv2.getTextSize(self.text, self.font_letter, self.font_scale, self.font_thickness)[0]
+        self.width_text = self.text_size[0]
+        self.height_text = self.text_size[1]
+
+    def show_letter(self, source, placeholder, rel_x, rel_y):
+        text_x = int((placeholder.width - self.width_text) / 2) + rel_x
+        text_y = int((placeholder.height + self.height_text) / 2) + rel_y
+        cv2.putText(source, self.text, (text_x, text_y), self.font_letter, self.font_scale, (255, 0, 0), self.font_thickness)
--- src/virtual_keyboard.py
+++ src/virtual_keyboard.py
@@ -1,11 +1,46 @@
 import cv2
 import numpy as np
+from keyboard.keyboard import KeyboardBasis as kb
 
-keyboard = np.zeros((1000, 1500, 3), np.uint8)
+#1 rectangle size
+x = 150
+y = 150
+height_ratio = 6
+width_ratio = 8
+#screen size = (a*b) * height_ratio * width_ratio
 
-#keys
-cv2.rectangle(keyboard, (0, 0), (200, 200), (255, 0, 0), 3)
+kb_settings = {'width': width_ratio * x, 'height': height_ratio * y, 'un': 3}
+rec_settings = {'height': y, 'width': x, 'thickness': 3, 'color': (255, 0, 0)}
+letter_settings = {'font_scale': 10, 'font_thickness': 4}
 
-cv2.imshow("keyboard", keyboard)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+kb_obj = kb(kb_settings)
+keyboard = kb_obj.get_background()
+
+letters_rus = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'К', 'Л']
+letters_eng = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']
+
+def letter(x, y, text):
+    # Keys
+    width = 200
+    height = 200
+    th = 3 # thickness
+    cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
+
+    # Text settings
+    font_letter = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 10
+    font_th = 4
+    text_size = cv2.getTextSize(text, font_letter, font_scale, font_th)[0]
+    width_text, height_text = text_size[0], text_size[1]
+    text_x = int((width - width_text) / 2) + x
+    text_y = int((height + height_text) / 2) + y
+    cv2.putText(keyboard, text, (text_x, text_y), font_letter, font_scale, (255, 0, 0), font_th)
+
+
+# letter(0, 0, "A")
+# letter(200, 0, "B")
+# letter(400, 0, "C")
+#kb_obj.set_letter_placeholder(rec_settings)
+
+kb_obj.set_letters(rec_settings, letter_settings, letters_eng)
+kb_obj.show_keyboard()
